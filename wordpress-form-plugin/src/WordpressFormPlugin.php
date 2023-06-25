1d5a99c6147af34c5c004273a8da1be51fd17e62
Kamil Pietrzkiewicz <seotulipplkamil@gmail.com>
2023-06-26 01:18:17 +0200

    Add uninstall and install config

--- wordpress-form-plugin/src/WordpressFormPlugin.php
+++ wordpress-form-plugin/src/WordpressFormPlugin.php
@@ -2,25 +2,30 @@
 
 namespace Kp;
 
-use Kp\Install\Application\CreateDatabaseTables;
+use Kp\Setup\Application\CreateDatabaseTables;
+use Kp\Setup\Application\RemoveDatabaseTables;
 use Kp\Shortcode\Application\RegisterShortCodes;
 
 class WordpressFormPlugin
 {
     private CreateDatabaseTables $createDatabaseTables;
+    private RemoveDatabaseTables $removeDatabaseTables;
     private RegisterShortCodes $registerShortCodes;
 
     public function __construct(
         CreateDatabaseTables $createDatabaseTables,
+        RemoveDatabaseTables $removeDatabaseTables,
         RegisterShortCodes $registerShortCodes
     ) {
         $this->createDatabaseTables = $createDatabaseTables;
+        $this->removeDatabaseTables = $removeDatabaseTables;
         $this->registerShortCodes = $registerShortCodes;
     }
 
-    public function run()
+    public function run(string $mainFilePath)
     {
-        $this->createDatabaseTables->create();
+        $this->createDatabaseTables->create($mainFilePath);
+        $this->removeDatabaseTables->removeTables($mainFilePath);
         $this->registerShortCodes->register();
     }
 }
