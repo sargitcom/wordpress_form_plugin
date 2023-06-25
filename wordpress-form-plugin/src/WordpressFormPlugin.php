adc65d6854c060a46ab65e0357d18b895b54ab44
Kamil Pietrzkiewicz <seotulipplkamil@gmail.com>
2023-06-25 23:13:46 +0200

    Update plugin start script

--- wordpress-form-plugin/src/WordpressFormPlugin.php
+++ wordpress-form-plugin/src/WordpressFormPlugin.php
@@ -2,20 +2,25 @@
 
 namespace KP;
 
+use KP\Install\Application\CreateDatabaseTables;
 use KP\Shortcode\Application\RegisterShortCodes;
 
 class WordpressFormPlugin
 {
+    private CreateDatabaseTables $createDatabaseTables;
     private RegisterShortCodes $registerShortCodes;
 
     public function __construct(
+        CreateDatabaseTables $createDatabaseTables,
         RegisterShortCodes $registerShortCodes
     ) {
+        $this->createDatabaseTables = $createDatabaseTables;
         $this->registerShortCodes = $registerShortCodes;
     }
 
     public function run()
     {
+        $this->createDatabaseTables->create();
         $this->registerShortCodes->register();
     }
 }
