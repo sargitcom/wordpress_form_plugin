585fd18ce5eb40fed5e1d44a4402e2f49e70eebb
Kamil Pietrzkiewicz <seotulipplkamil@gmail.com>
2023-06-26 01:53:46 +0200

    Fix issue with not removing vendor folder

--- wordpress-form-plugin/uninstall.php
+++ wordpress-form-plugin/uninstall.php
@@ -1,12 +1,10 @@
 <?php
 
-require_once(__DIR__ . '/vendor/autoload.php');
-
 if (!defined('WP_UNINSTALL_PLUGIN')) exit();
 
 global $wpdb;
 
-$tableName = $wpdb->prefix . \Kp\Setup\Infrastructure\Wordpress\WordpressTablesRepository::FORM_ENTRIES_TABLE_NAME;
+$tableName = $wpdb->prefix . 'wordpress_form_plugin_entries';
 
 $sql = <<<SQL
 DROP TABLE IF EXISTS $tableName;  
