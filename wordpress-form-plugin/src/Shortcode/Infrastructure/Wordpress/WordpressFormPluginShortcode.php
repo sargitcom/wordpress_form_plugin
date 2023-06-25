b4153abd08f1f74f654bcb7f45cc80d2ad9d4e53
Kamil Pietrzkiewicz <seotulipplkamil@gmail.com>
2023-06-25 23:35:17 +0200

    Update html of a form shortcode

--- wordpress-form-plugin/src/Shortcode/Infrastructure/Wordpress/WordpressFormPluginShortcode.php
+++ wordpress-form-plugin/src/Shortcode/Infrastructure/Wordpress/WordpressFormPluginShortcode.php
@@ -13,24 +13,34 @@ class WordpressFormPluginShortcode implements FormPluginShortcode
 
     public function registerWordpressFormShortcode() : string
     {
+        $subject = \esc_html__('Subject', 'wordpress-form-plugin');
+        $firstName = \esc_html__('First name', 'wordpress-form-plugin');
+        $lastName = \esc_html__('Last name', 'wordpress-form-plugin');
+        $email = \esc_html__('Email', 'wordpress-form-plugin');
+        $message = \esc_html__('Message', 'wordpress-form-plugin');
+        $submit = \esc_html__('Submit', 'wordpress-form-plugin');
+
         return <<<SHORTCODE
 <div class="kp-wordpress-form">
     <form class="kp-wordpress-form-handle" method="post" action="">
         <div class="kp-wordpress-form-field">
-            <input class="kp-wordpress-form-input" type="text" name="subject" />
+            <input class="kp-wordpress-form-input" type="text" name="subject" placeholder="$subject" />
         </div>        
         <div class="kp-wordpress-form-field">
-            <input class="kp-wordpress-form-input" type="text" name="first_name" />
+            <input class="kp-wordpress-form-input" type="text" name="first_name" placeholder="$firstName" />
         </div>
         <div class="kp-wordpress-form-field">
-            <input class="kp-wordpress-form-input" type="text" name="last_name" />
+            <input class="kp-wordpress-form-input" type="text" name="last_name" placeholder="$lastName" />
         </div>
         <div class="kp-wordpress-form-field">
-            <input class="kp-wordpress-form-input" type="text" name="email" />
+            <input class="kp-wordpress-form-input" type="text" name="email" placeholder="$email" />
         </div>
         <div class="kp-wordpress-form-field">
-            <input class="kp-wordpress-form-input" type="text" name="message" />
-        </div>    
+            <textarea class="kp-wordpress-form-input" type="text" name="message" placeholder="$message"></textarea>
+        </div>
+        <div class="kp-wordpress-form-submit">
+            <button type="submit">$submit</button>
+        </div>
     </form>
 </div>
 SHORTCODE;;
